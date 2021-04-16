7364dea28154543cd957a4500de507963bc9ae38
aidan <aidanguarniere@gmail.com>
2021-04-16 12:47:49 -0700

    me query w dual paramters

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+// requirements
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({
+          $or: [{ _id: context.user._id }, { username: context.user.username }],
+        }).select("-__v -password");
+
+        return userData;
+      }
+      throw new AuthenticationError("Not Logged in");
+    },
+  },
+};
