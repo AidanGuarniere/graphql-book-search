6138e52772d08463d238166c4aadb4981e461a41
aidan <aidanguarniere@gmail.com>
2021-04-17 16:23:13 -0700

    connected gql to client side, cannot figure out why my jwt is broken

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 // Server
 const PORT = process.env.PORT || 3001;
